93244e3f56873128d3102c4bc7a36b2afa21cbcc
Songj2 <55727461+Songj2@users.noreply.github.com>
2022-01-20 14:55:30 +0900

    GREEDY_강의 풀이
    *수열로 생각못함..
    *단순하게 생각하기에서 멈추지 말고 수열도 생각해보기

--- Greedy_RuleOfBigNumber.py
+++ Greedy_RuleOfBigNumber.py
@@ -17,4 +17,22 @@ for i in range(m):
         kCount= k #연속 횟수 초기화
 
 print(result)
-# 배열에서 가장 큰 숫자를 k만큼 더한 뒤, 두번째로 큰 숫자를 더하고 반복하면 가장 큰 수를 만들 수 있음
+# 배열에서 가장 큰 숫자를 k만큼 더한 뒤, 두번째로 큰 숫자를 한번 더하기를 반복하면 가장 큰 수를 만들 수 있음
+#####################################################################
+# 반복되는 수열에 대해 파악하여 시간복잡도를 줄일 수 있음
+n, m, k= map(int, input().split())
+data= list(map(int, input().split()))
+
+data.sort() 
+first= data[n-1] #가장 큰 수
+second= data[n-2] # 두 번째로 큰 수
+
+#가장 큰 수가 더해지는 횟수 계산
+count= int(m/(k+ 1))* k #(K+ 1)의 반복
+count+= m% (k+ 1) #m이 (k+ 1)로 나눠 떨어지지 않는 경우도 계산
+
+result= 0
+result+= (count)* first #가장 큰 수 더하기
+result++ (m- count)* second #두 번째로 큰 수 더하기
+
+print(result)
