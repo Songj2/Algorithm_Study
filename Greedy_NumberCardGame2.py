c8dd27bf67a12057ccca54f0ae048c5120d966b4
Songj2 <55727461+Songj2@users.noreply.github.com>
2022-01-21 11:22:23 +0900

    GREEDY_강의 풀이
    *한줄씩 입력받는 형태이므로 입력받고 바로 확인가능함

--- Greedy_NumberCardGame2.py
+++ Greedy_NumberCardGame2.py
@@ -12,3 +12,33 @@ for i in range(n):
         result= minValue #더 큰값 저장
     
 print(result)
+#######################################################
+# 각 행마다 가장 작은 수를 찾은 뒤, 그 수 중에서 가장 큰 수 찾기
+# min()함수나 2중 반복문 구조를 이용할 수 있어야 함
+#min() ver.
+n, m= map(int, input().split())
+
+result= 0
+for i in range(n):
+    data= list(map(int,input().split()))
+    #현재 줄에서 가장 작은 수 찾기
+    min_Value= min(data) 
+    #가장 작은 수들 중에서 가장 큰 수 찾기
+    result= max(result, min_Value)
+
+print(result)
+
+#2중 반복문 ver.
+n, m= map(int, input().split())
+
+result= 0
+for i in range(n):
+    data= list(map(int,input().split()))
+    #현재 줄에서 가장 작은 수 찾기
+    min_Value= 10001 #입력 조건에서 입력으로 들어오는 수는 10000이하의 수 이므로
+    for a in data:
+        min_Value= min(min_Value, a)
+    #가장 작은 수들 중에서 가장 큰 수 찾기
+    result= max(result, min_Value)
+
+print(result)
