78d20dff60e3c72a20337491318e34c6fe6d4a2d
Songj2 <55727461+Songj2@users.noreply.github.com>
2022-01-25 14:19:07 +0900

    IMPLEMENTATION_강의 풀이
    * 답안과 유사하다!
    *더 줄일 필요가 없었다^^

--- IMPLEMEMTAION_RoyalKnight.py
+++ IMPLEMEMTAION_RoyalKnight.py
@@ -10,3 +10,20 @@ for i in move:
         # print(i, chr(x+ i[1]+ 97), y+ +i[0]+ 1)
 
 print(count)
+###################################################################################
+input_data= input()
+row= int(input_data[1])
+column= int(ord(input_data[0]))- int(ord('a'))+1
+
+# 나이트가 이동할 수 있는 방향 정의
+steps= [(-2,-1),(-2,1), (-1,2), (1,2), (2, 1), (2, -1), (1, -2), (-1, -2)]
+
+result= 0
+#이동하고자 하는 위치 확인
+for step in steps:
+    next_row= row+ step[0]
+    next_column= column+ step[1]
+    # 해당 위치로 이동이 가능하다면 카운트 증가
+    if next_row>= 1 and next_row<= 8 and next_column>= 1 and next_column<= 8:
+        result+= 1
+print(result)
