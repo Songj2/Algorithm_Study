41fca2970a70baa1838985f461dc1e6407cdaecf
Songj2 <55727461+Songj2@users.noreply.github.com>
2022-01-27 13:01:54 +0900

    IMPLEMENTATION_강의 풀이
    *풀이는 비슷한 편
    * 뭔가 아쉽다...

--- IMPLEMENTATION_GameDevelop.py
+++ IMPLEMENTATION_GameDevelop.py
@@ -38,4 +38,61 @@ while True:
         break
         
 print(count)
-    
+###########################################################################################
+# 방향백터를 이용하는 것이 효과적
+# #리스트 컴프리헨션 문법을 사용해 2차원 리스트를 초기화_2차원 리스트를 선언할 때 컴프리헨션을 이용하는 것이 효율적
+# 왼쪽으로 회전: turn_left()에서 global키워드 사용_ 정수형 번수인 direction변수가 함수 바깥에서 선언된 전역 변수이기 때문
+#     
+n,m= map(int, input().split())
+#방문한 위치를 저장하기 위한 맵을 생성하여 0으로 초기화
+d= [[0]*m for _ in range(n)]
+
+x, y, direction= map(int, input().split())
+d[x][y]= 1 #현재 좌표 방문 처리
+array= []
+#전체 맵 정보 입력받기
+for i in range(n):
+    array.append(list(map(int, input().split())))
+#북 ,동, 남, 서 방향 정의
+dx= [-1, 0, 1, 0]
+dy= [0, 1, 0, -1]
+
+#왼쪽으로 회전
+def turn_left():
+    global direction
+    direction-= 1
+    if direction== -1:
+        direction= 3
+
+#시물레이션
+count= 1
+turn_time= 0
+while True:
+    #왼쪽 회전
+    turn_left()
+    nx= x+ dx[direction]
+    ny= y+ dy[direction]
+    #회전한 후 정면에 가보지 않은 칸이 존재하는 경우 이동
+    if d[nx][ny]== 0 and array[nx][ny]== 0:
+        d[nx][ny]= 1
+        x= nx
+        y= ny
+        count+= 1
+        turn_time= 0
+        continue
+    #회전한 후 정면에 가보지 않은 칸이 없거나 바다인 경우
+    else:
+        turn_time+= 1
+    if trun_time== 4:
+        nx= x-dx[direction]
+        ny= y-dy[direction]
+
+        if array[nx][ny]== 0:
+            x= nx
+            y= ny
+        #뒤가 바다로 막혀 있는 경우
+        else:
+            break
+        turn_time= 0
+
+print(count)    
