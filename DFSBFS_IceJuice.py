7650656b2215444843c2f4f3afea1f261e52ddb6
Songj2 <55727461+Songj2@users.noreply.github.com>
2022-01-29 13:08:31 +0900

    Update_MyCode
    * 사용하지 않은 인자 제거

--- DFSBFS_IceJuice.py
+++ DFSBFS_IceJuice.py
@@ -29,6 +29,38 @@ for i in range(n):
     
 print(count)
 ################################################################
+#사용하지 않은 인자 제거
+n, m= map(int, input().split())
+frame= []
+for i in range(n):
+    frame.append(list(map(int, input())))
+
+count= 0
+way= [(0, 1), (1, 0), (0, -1), (-1, 0)]
+visited= frame
+
+def check(x, y, frame, visited):
+    if frame[x][y]== 0:
+        visited[x][y]= 3 #방문 처리
+        # print("Count", count, x, y)
+        
+        for i in way:
+            nx= x+i[0]
+            ny= y+i[1]
+            if 0<= nx< n and 0<= ny< m: #주변 확인
+                check(nx, ny, frame, visited)
+    else:
+        return 0
+
+    return 1  
+
+for i in range(n):
+    for j in range(m):
+        if visited[i][j]== 0: #방문하지 않은 칸이면 주변 확인
+            count+= check(i, j, frame, visited)
+    
+print(count)
+################################################################
 n, m= map(int, input().split())
 graph= []
 for i in range(n):
