1d4c0d5f414651994f30f6bee0d52ec47aa5c853
Songj2 <55727461+Songj2@users.noreply.github.com>
2022-02-09 11:00:38 +0900

    SORT_강의 풀이
    만족!

--- SORT_Grade.py
+++ SORT_Grade.py
@@ -5,5 +5,18 @@ for i in range(n):
     data.append((tn, int(tg)))
 
 data.sort(key= lambda x:x[1])
+
 for i in range(n):
     print(data[i][0], end=" ")
+
+##########################################
+n= int(input())
+array= []
+for i in range(n):
+    input_data= input().split()
+    array.append((input_data[0], int(input_data[1])))
+
+array= sorted(array, key= lambda stuednt:stuednt[1])
+
+for student in array:
+    print(student[0], end=" ")   
