1be41471d8cb1be1da923eb49347f8daad3e9243
Songj2 <55727461+Songj2@users.noreply.github.com>
2022-02-13 14:52:14 +0900

    BinarySearch_ MyCodeUpdate
    * 이진탐색으로 작성해봄

--- BinarySearch_TteokCutting.py
+++ BinarySearch_TteokCutting.py
@@ -31,3 +31,36 @@ while rest>= m:
 
 
 print(h)
+######################################################################
+n, m= map(int, input().split())
+tteok= list(map(int, input().split()))
+tteok.sort()
+
+def cutting(data, m, start, end):
+    if start> end:
+        return None
+
+    mid= (start+end)//2
+    temp= 0
+    for i in range(mid, end):
+        if data[i]-data[mid]>0:
+            temp+= data[i]-data[mid]
+    
+    if temp<m:
+        return start, mid
+    else:
+        return cutting(data, m, mid+1, end)
+
+ss, mm= cutting(tteok, m, 0, n)
+h= tteok[ss]
+result= tteok[-1]
+
+while result>m:
+    h+=1
+    result= 0
+    
+    for i in range(ss, n):
+        if tteok[i]-h> 0:
+            result+=tteok[i]- h
+
+print(h)      
