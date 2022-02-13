f3832cd32c5ecd97cc474af2a110498858bb7acc
Songj2 <55727461+Songj2@users.noreply.github.com>
2022-02-13 15:12:15 +0900

    BinarySearch_ 강의 풀이
    * 더 많은 공부가 필요하다..

--- BinarySearch_TteokCutting.py
+++ BinarySearch_TteokCutting.py
@@ -64,3 +64,29 @@ while result>m:
             result+=tteok[i]- h
 
 print(h)      
+######################################################################
+n, m= list(map(int, input().split(" ")))
+array= list(map(int, input().split()))
+
+#시작점과 끝점 설정
+start= 0
+end= max(array)
+
+#이진 탐색 수행
+result= 0
+while(start<= end):
+    total= 0
+    mid=(start+end)//2
+    for x in array:
+        #잘랐을 때의 떡의 양 계산
+        if x> mid:
+            total+= x- mid
+    #떡의 양이 부족한 경우 더 자르기_ 왼쪽 탐색
+    if total< m:
+        end= mid- 1
+    #떡의 양이 충분한 경우 덜 자르기_ 오른쪽 탐색
+    else:
+        result= mid #최대한 덜 자른 값을 구하므로 result에 기록
+        start= mid+ 1
+
+print(result)
