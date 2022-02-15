5a2215f78ed1581657cc09a83427a8b6a37e5731
Songj2 <55727461+Songj2@users.noreply.github.com>
2022-02-15 19:00:12 +0900

    BinarySearch_강의 풀이
    *직접 구현이 아닌 bisect를 이용함

--- BinarySearch_Count.py
+++ BinarySearch_Count.py
@@ -30,5 +30,29 @@ def search(data, x, start, end):
     else:
         return search(data, x, mid+1, end)
 ss, ee=  search(data, x, 0, n)
-result= ee-ss+1
+if ee-ss>0:
+    result= ee-ss+1
+else:
+    result= -1
 print(result)
+
+########################################################
+#lectureCode
+from bisect import bisect_left, bisect_right
+
+#값이 [left_value, right_value]인 데이터의 개수를 반환하는 함수
+def countByRange(array, left_value, right_value):
+    right_index= bisect_right(array, right_value)
+    left_index= bisect_left(array, left_value) 
+    return right_index- left_index
+
+n, x= map(int, input().split())
+array= list(map(int, input().split()))
+
+#범위에 있는 데이터의 개수 계산
+count= countByRange(array, x, x)
+
+if count== 0:
+    print(-1)
+else:
+    print(count)
