9f8875a430ca2324ce995d88b22b494f301ab5da
Songj2 <55727461+Songj2@users.noreply.github.com>
2022-02-28 11:45:13 +0900

    DynamicProgramming_강의 풀이

--- DynamicProgramming_AntWarrior.py
+++ DynamicProgramming_AntWarrior.py
@@ -17,3 +17,15 @@ for i in range(2, n):
     dp[i]= dp[i-2]+food[i]
 
 print(max(dp))
+################################################
+n= int(input())
+array= list(map(int, input().split()))
+#dp테이블 초기화
+d= [0]*100
+#bottom-up방식
+d[0]= array[0]
+d[1]= max(array[0], array[1])
+for i in range(2, n):
+    d[i]= max(d[i-1], d[i-2]+ array[i])
+
+print(d[n-1])
