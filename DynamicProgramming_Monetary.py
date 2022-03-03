434f63e3cd07c1b6f2d416cd366de7596e689788
Songj2 <55727461+Songj2@users.noreply.github.com>
2022-03-03 11:35:50 +0900

    DynamicProgramming_강의 풀이
    *틀렸다
    *어렵다

--- DynamicProgramming_Monetary.py
+++ DynamicProgramming_Monetary.py
@@ -17,4 +17,22 @@ if m!= 0:
     print("-1")
 else:
     print(count)
+######################################################
+n, m= map(int, input().split())
+array= []
+for i in range(n):
+    array.append(int(input()))
 
+d=[10001]*(m+1)
+#Bottom-up 방식
+d[0]= 0
+for i in range(n):
+    for h in range(array[i], m+1):
+        #(i- k)원을 만드는 방법이 존재하는 경우
+        if d[j-array[i]]!= 10001:
+            d[j]= min(d[j], d[j-array[i]]+ 1)
+
+if d[m]== 10001:
+    print(-1)
+else:
+    print(d[m])
