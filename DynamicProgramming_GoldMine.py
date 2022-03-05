ce732bbfa948a65af9e2129d0830599b0ad66619
Songj2 <55727461+Songj2@users.noreply.github.com>
2022-03-05 15:09:47 +0900

    DynamicPRogramming_강의 풀이

--- DynamicProgramming_GoldMine.py
+++ DynamicProgramming_GoldMine.py
@@ -19,7 +19,7 @@ for i in range(t):
                     maxInd= j
         
         result.append((maxInd, maxGold))
-    print(mine, "\n", result)
+    
     gold= 0
     for i in result:
         gold+= i[1]
@@ -27,9 +27,32 @@ for i in range(t):
     # print(gold)
 for i in range(t):
     print(array[i], end="\n")
+#######################################################################
+for tc in range(int(input())):
+    # 금광 정보 입력
+    n, m= map(int, input().split())
+    array=list(map(int, input().split()))
+    
+    # DP테이블 초기화
+    dp= []
+    index= 0
+    for i in range(n):
+        dp.append(array[index:index+m])
+        index+= m
 
-# 2 
-# 3 4
-# 1 3 3 2 2 1 4 1 0 6 4 7
-# 4 4
-# 1 3 1 5 2 2 4 1 5 0 2 3 0 6 1 2
+    # 다이나믹 프로그래밍 진행
+    for j in range(1, m):
+        for i in range(n):
+            # 완쪽 상단에서 오는 경우
+            if i== 0: left_up= 0
+            else: left_up= dp[i-1][j-1]
+            # 왼쪽 하단에서 오는 경우
+            if i== n-1: left_down= 0
+            else: left_down= dp[i+1][j-1]
+            # 왼쪽에서 오는 경우
+            left= dp[i][j-1]
+            dp[i][j]= dp[i][j]+ max(left_up, left_down, left)
+    result= 0
+    for i in range(n):
+        result= max(result, left_up, left_down, left)
+    print(result)
