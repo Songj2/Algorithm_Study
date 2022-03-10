93957013f143abc86eea2f55bc36ffe8f91ad6c1
Songj2 <55727461+Songj2@users.noreply.github.com>
2022-03-10 16:37:57 +0900

    DynamicProgramming_강의 풀이
    * LIS

--- DynamicProgramming_ArrangementSoldier copy.py
+++ DynamicProgramming_ArrangementSoldier copy.py
@@ -13,3 +13,21 @@ while i< n:
         count+=1
     i+= 1
 print(count)
+##########################################################
+# 가장 긴 증가하는 부분 수열(Longest Incresing Subsequence, LIS)의 전형적인 다이나밍 프로그램이 문제\
+# 가장 긴 감소하는 부분 수열을 찾는 문제
+# 점화식: D[i]= max(D[i], D[j]+ 1) if array[j]< array[i]
+n= int(input())
+array= list(map(int, input().split()))
+# 순서를 오름차순으로 뒤집어, 최장 증가하는 부분 수열(LIS)로 해결
+array.reverse()
+
+dp= [1]*n
+
+# LIS 수행
+for i in range(1, n):
+    for j in range(0, i):
+        if array[j]< array[i]:
+            dp[i]= max(dp[i], dp[j]+ 1)
+
+print(n-max(dp))
